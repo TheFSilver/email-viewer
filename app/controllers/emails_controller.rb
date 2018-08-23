2120c6be1bd4d7572948838f7a50b9f69e212247
skageraz <skageraz@gmail.com>
2018-08-23 20:52:47 +0200

    delete fonctionnel

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,28 @@
+class EmailsController < ApplicationController
+  before_action :all_emails, only: [:index, :destroy]
+	before_action :set_emails, only: [:destroy, :show]
+	respond_to :html, :js
+
+
+    def show
+  @email.read = true
+  @email.save
+end
+
+
+  def destroy
+    @emails = Email.destroy(params[:id])
+    redirect_to root_path
+  end
+
+
+private
+def all_emails
+    @emails = Email.all
+  end
+
+  def set_emails
+    @email = Email.find(params[:id])
+  end
+
+end
